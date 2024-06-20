d66849d07fdb0da8b5c291b734211f3540d9cea6
Omotola Odusanya <odusanyaomotola63@gmail.com>
2024-06-20 16:48:19 +0100

    Large Screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
     extend: {
       colors: {
         "gray-1": "#6A6A70",
+        "gray-2": "#39393F",
+        "gray-3": "#BBBBBB",
+        "green-4": "#00BF69",
+        "white-1": "#FCFCFC",
       },
     },
   },
